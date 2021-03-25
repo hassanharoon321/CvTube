f009c59e50a755980918a3fbd41b1a67ad27d946
hassan haroon <hassanharoon321@gmail.com>
2021-03-25 17:53:09 +0500

    adeed

---
+++ Routing/src/App.js
@@ -0,0 +1,48 @@
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Home from "./Components/Home";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ privacyPolicy/src/App.js
@@ -0,0 +1,14 @@
+import PrivacyPolicy from "./Components/Privacy-Policy/All-Work/PrivacyPolicy";
+import TermsAndCondition from "./Components/Terms-and-conditions/All-Work/TermsAndCondition"
+
+
+function App() {
+  return (
+    <div>
+      <PrivacyPolicy />
+      <TermsAndCondition/>
+    </div>
+  );
+}
+
+export default App;
